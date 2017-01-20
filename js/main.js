4b4e76fd9f83325e2514ee9d6f7961f94f67bb0b
Vagalep <ivagalep@gmail.com>
2017-01-20 16:52:03 +0200

    change logic self conclusion

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ function displayRailPanel(){
 
 function selectFunction(){
 	if (resultIn.value > 0){
-		$('#conclusionIn').html('<option value="Fixed">Fixed</option>');
+		$('#conclusionIn').html('<option value="Fixed">Fixed</option><option value="Fixedddd">Fixedddd</option>');
 	} else if (resultIn.value < 0){
 		$('#conclusionIn').html('<option value="Reproduced">Reproduced</option>');
 	}else{
@@ -25,20 +25,34 @@ function selectFunction(){
 function generateHeader() {
 	var resultIn = document.getElementById("resultIn");
 	var conclusionIn = document.getElementById("conclusionIn");
-	var selfConclusionIn = document.getElementById(selfConclusionIn);
-	console.log(selfConclusionIn);
-	if (resultIn.value > 0){
+	var selfConclusionIn = document.getElementById("selfConclusionIn");
+	console.log(selfConclusionIn.value.length);
+	if (selfConclusionIn.value.length == 0){
+		console.log("1");
+		if (resultIn.value > 0){
 		var rezultFormat = "{panel:title=Result:|titleColor=#004d1a|borderColor=#0E6251|titleBGColor=#4dff88|bgColor=#ccffdd}";
-		var conclusionFormat = "| (i) *Conclusion:* | Fixed (/) |<br>{panel}";
-	}else if (resultIn.value < 0){
+		var conclusionFormat = "| (i) *Conclusion:* | " + conclusionIn.value + " (/) |<br>{panel}";
+		}else if (resultIn.value < 0){
 		var rezultFormat = "{panel:title=Result:|titleColor=#662900|borderColor=#922B21|titleBGColor=#ff6600|bgColor=#ff8533}"
-		var conclusionFormat = "| (i) *Conclusion:* | Reproduced (x) |<br>{panel}";
-	}else if (resultIn.value == 0){
+		var conclusionFormat = "| (i) *Conclusion:* | " + conclusionIn.value + " (x) |<br>{panel}";
+		}else if (resultIn.value == 0){
 		var rezultFormat = "{panel:title=Result:|titleColor=#000000|borderColor=#000000|titleBGColor=#ccc3ae|bgColor=#fff4da}"
-		var conclusionFormat = "| (i) *Conclusion:* | Can't reproduce (x) |<br>{panel}";
+		var conclusionFormat = "| (i) *Conclusion:* | " + conclusionIn.value + " (x) |<br>{panel}";
+		}
 	}else{
-		var conclusionFormat = "| (i) *Conclusion:* | " + selfConclusionIn.value + " |<br>{panel}";
+		console.log("2");
+		if (resultIn.value > 0){
+		var rezultFormat = "{panel:title=Result:|titleColor=#004d1a|borderColor=#0E6251|titleBGColor=#4dff88|bgColor=#ccffdd}";
+		var conclusionFormat = "| (i) *Conclusion:* | " + selfConclusionIn.value + " (/) |<br>{panel}";
+		}else if (resultIn.value < 0){
+		var rezultFormat = "{panel:title=Result:|titleColor=#662900|borderColor=#922B21|titleBGColor=#ff6600|bgColor=#ff8533}"
+		var conclusionFormat = "| (i) *Conclusion:* | " + selfConclusionIn.value + " (x) |<br>{panel}";
+		}else if (resultIn.value == 0){
+		var rezultFormat = "{panel:title=Result:|titleColor=#000000|borderColor=#000000|titleBGColor=#ccc3ae|bgColor=#fff4da}"
+		var conclusionFormat = "| (i) *Conclusion:* | " + selfConclusionIn.value + " (x) |<br>{panel}";
+		}
 	}
+
 	
 	var resultOut = document.getElementById("resultOut");
 	var conclusionOut = document.getElementById("conclusionOut");
